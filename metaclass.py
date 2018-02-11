811a9279b55e200148f4e0589ca5068b307d301b
Tendai Mutunhire <tmutunhire@gmail.com>
2018-02-11 22:42:09 +0200

    add attributes to classes

---
+++ metaclass.py
@@ -0,0 +1,20 @@
+class AttributeInitType(type):
+    def __call__(self, *args, **kwargs):
+        """Create a new instance"""
+
+        # create the object in the normal default way
+        obj = type.__call__(self, *args)
+
+        # set attributes on the new object
+        for name, value in kwargs:
+            setattr(obj, name, value)
+
+        # return the new object
+        return obj
+
+class Car(object, metaclass=AttributeInitType):
+
+    @property
+    def description(self):
+        """Return a description of this car"""
+        return " ".join(str(value) for value in self.__dict__.values())
